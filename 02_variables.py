5f988264cf76290af100c73ecc34fe418070a63c
Apurva Waghmare <apurvawaghmare009@gmail.com>
2025-09-01 12:38:09 +0530

    create variables

---
+++ 02_variables.py
@@ -0,0 +1,23 @@
+a = 12
+b = 8
+print(a + b)
+
+a = "apurva" # a is a String
+b = 18 # b is a integer
+c = 3.14 # c is a floating point number
+d = True # d is a boolean variable
+e = None # e is a None type variable
+
+name = "apurva"
+first_number = 35
+
+x = 10
+print(id(x)) # id is the reference to where the value is stored
+
+# Reusability in Python
+x = "apurva"
+print(id(x))
+
+y = "apurva"
+print(id(y))
+
