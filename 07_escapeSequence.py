ec70d4398a1a3608b91fdaae569b0d9c3a648972
Apurva Waghmare <apurvawaghmare009@gmail.com>
2025-09-01 12:57:15 +0530

    escape sequence characters commit

---
+++ 07_escapeSequence.py
@@ -0,0 +1,14 @@
+name = "I am Iron Man\nI am Tony Stark"
+print(name)
+
+name2 = "I am\tBatman"
+print(name2)
+
+name3 = "I am \"Batman\""
+print(name3)
+
+name4 = 'I am \'Superman\''
+print(name4)
+
+name5 = "This is \\Python programming..."
+print(name5)
