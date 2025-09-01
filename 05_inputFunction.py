ecd504d3f3c4fdee7204fee7ee01d33ce69f02d4
Apurva Waghmare <apurvawaghmare009@gmail.com>
2025-09-01 12:55:51 +0530

    user input commit

---
+++ 05_inputFunction.py
@@ -0,0 +1,10 @@
+#Taking user input
+a = input("Enter first number: ")
+b = input("Enter second number: ")
+print("The sum of a and b is:", a+b)
+print(type(a))
+
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+print("The sum of x and y is:", x+y)
+print(type(x))
