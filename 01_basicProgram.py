95eb070fb5ad65c88bf9d0d10eb788a524421705
Apurva Waghmare <apurvawaghmare009@gmail.com>
2025-09-01 12:37:04 +0530

    Create python program

---
+++ 01_basicProgram.py
@@ -0,0 +1,18 @@
+print("Hello World...")
+
+# This is a single line comment
+# I am Iron Man!!!
+
+''' This is a multiline 
+comment in Python '''
+""" This is also a
+multiline comment in Pyhton """
+
+print(2+3)
+
+print("This is" + " Python program...")
+
+print('''Twinkle Twinkle, little star,
+      how I wounder what you are!
+      up above the world so high
+      like a diamond in the sky''')
