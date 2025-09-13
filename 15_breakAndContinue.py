39e67eefce62cc80ae36573ccfc80948e94029fd
Apurva Waghmare <apurvawaghmare009@gmail.com>
2025-09-13 13:17:46 +0530

    create break and continue

---
+++ 15_breakAndContinue.py
@@ -0,0 +1,13 @@
+# Break Statement
+for i in range(0, 50):
+    if i == 3:
+        break # end the loop if condition is satisfied
+    print(i)
+print("--------------------------------")
+
+for i in range(0, 50):
+    if i == 3:
+        continue # skip this and continue to next iteration
+    print(i)
+
+print("end of program...")
---
+++ 16_patternPrinting.py
@@ -0,0 +1,10 @@
+n = int(input("Enter a number: "))
+# for i in range(1,11):
+#     print(n * i)
+
+a = 10>=5
+print(a)
+print(type(a))
+
+print(True==1)
+print("End of program...")
