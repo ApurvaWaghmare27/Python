993f108e89a186c39f4e9aa920bf64d39a79836e
Apurva Waghmare <apurvawaghmare009@gmail.com>
2025-09-13 13:16:43 +0530

    create loops

---
+++ 13_whileLoop.py
@@ -0,0 +1,15 @@
+i = 1
+while i <= 50:
+    print(i)
+    i += 1
+
+print("End of program...")
+
+# Print elements of list using while loop
+l1 = ["rohan", 3.14, 2, False, "Nagpur", 1000]
+
+i = 0
+while (i < len(l1)):
+    print(l1[i])
+    i += 1
+print("end of program...")
---
+++ 14_forLoop.py
@@ -0,0 +1,29 @@
+# for i in range(6):
+#     print(i)
+
+# for i in range(2, 22, 2):
+#     print(i)
+
+# For loop with Lists
+L1 = [2, 22, 3.14, 37, 1200]
+for i in L1:
+    print(i)
+
+# For loop with Tuples
+T1 = (12, 3, 22.3, False, "Rohan")
+for i in T1:
+    print(i)
+
+# For loop with Strings
+S1 = "Apurva"
+for i in S1:
+    print(i)
+
+# For loop with else
+L2 = [1, 7, 9]
+for i in L2:
+    print(i)
+else:
+    print("Done...")
+
+print("End of program...")
